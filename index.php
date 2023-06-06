55455c41b853d99c79fc9091cbb10873abeca752
yosky520 <129939238+yosky520@users.noreply.github.com>
2023-06-06 20:59:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> 舔狗日记，玩吧急救中心</title>
+		<meta name="keywords" content="舔狗日记,青山绿水,ins.play8.icu,毒鸡汤">
+        <meta name="description" content="ins.play8.icu这是一个舔狗日记集合网站,有毒，毒鸡汤">
+        <link rel="shortcut icon" href="favicon.ico">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="./static/css/main.css" />
+		<noscript><link rel="stylesheet" href="./static/css/noscript.css" /></noscript>
+	</head>
+	<body onselectstart="return false" oncontextmenu=self.event.returnValue=false> 
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span>舔狗日记</span>						</div>
+						<div class="content">
+							<div class="inner">
+								<h2><?php echo date("Y年m月d日D")?>   晴</h2>
+								<h3 id="sentence"><br>-------------------------------------<br>
+								<script src="api.php"></script></h3>
+							</div>
+						</div>
+                   <button class="btn btn-primary btn-filled btn-xs" onclick="randomSentence()">再来一段</button>
+						<nav>
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">©ins.play8.icu 版权所有<a target="_black" href='https://ins.play8.icu'>玩吧急救中心</a></p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="./static/js/jquery.min.js"></script>
+			<script src="./static/js/skel.min.js"></script>
+			<script src="./static/js/util.js"></script>
+			<script src="./static/js/main.js"></script>
+            <script src="./static/js/index.js"></script>
+
+ 
+
+	</body>
+</html>
